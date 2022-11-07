71d5805911549c04c9fd9aed9ea2c46b0f8b7e25
chenRF <895970721@qq.com>
2022-11-07 22:32:40 +0800

    first commit

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,10 @@
 public class Test {
     public Test() {
+        System.out.println("123");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("123");
+        System.out.println(123);
     }
 }
